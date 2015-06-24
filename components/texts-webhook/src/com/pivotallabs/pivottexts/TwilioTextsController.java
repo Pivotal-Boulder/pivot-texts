2f746bc1c69f2680c0af611dbae1ae9595a2399e
Joseph Rodriguez <jrodriguez@pivotal.io>
2015-06-24 16:58:10 -0600

    Add debug statements to webhook controller

--- components/texts-webhook/src/com/pivotallabs/pivottexts/TwilioTextsController.java
+++ components/texts-webhook/src/com/pivotallabs/pivottexts/TwilioTextsController.java
@@ -6,6 +6,7 @@ import com.twilio.sdk.verbs.Message;
 import com.twilio.sdk.verbs.TwiMLException;
 import com.twilio.sdk.verbs.TwiMLResponse;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,14 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @RestController
 public class TwilioTextsController {
+
+    private final Logger logger = getLogger(this.getClass());
 
     @Autowired
     PivotTextsService pivotTextsService;
 
     @RequestMapping(value = "/twilio-texts", method = RequestMethod.POST, produces = MediaType.TEXT_XML_VALUE)
     public ResponseEntity<String> receiveText(@ModelAttribute("From") String from, @ModelAttribute("Body") String body) throws IOException, TwiMLException {
+        logger.debug("Create a PivotText from: {}  body: {}", from, body);
+
         TwiMLResponse twiMLResponse = new TwiMLResponse();
 
         if (StringUtils.isEmpty(from) || StringUtils.isEmpty(body)) {
